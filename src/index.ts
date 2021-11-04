59ef8aa0be1fee8d63e70188066ef2ea9a594d86
NadiaElso <92063948+NadiaElso@users.noreply.github.com>
2021-11-04 20:52:04 -0300

    funcionando

--- src/index.ts
+++ src/index.ts
@@ -5,8 +5,8 @@ num[2] = 0;
 num[3] = 0;
 num[4] = 0;
 for (let i = 0; i < num.length; i++) {
-  num[i] = prompt("ingrese un numero");
+  num [i] = prompt("ingrese un numero");
 }
 for (let index = 0; index < num.length; index++) {
-  console.log("El numero en la posicion", num[index], "es", num[index]);
+  console.log(`El numero en la posicion,${index},es, ${num[index]}´);
 }
